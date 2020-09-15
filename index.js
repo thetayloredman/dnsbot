4e82c73f46820aba2e7d9dc1dd739ade15c8434e
thetayloredman <nutdriver716@gmail.com>
2020-09-15 16:00:17 -0700

    E lol

--- index.js
+++ index.js
@@ -114,5 +114,15 @@ client.on('message', (message) => {
     commandrun.run(client, message, args);
 });
 
+// Other logging events
+client.on('guildMemberAdd', (member) => {
+    log('i', `A new member joined! Name: "${member.user.tag}" ID: ${member.user.id}`);
+});
+client.on('guildCreate', (guild) => {
+    if (guild.id === client.config.guild.id) {return;}
+    log('w', `I was added to an UNAUTHORIZED GUILD: ${guild.name} (${guild.id}): LAUNCHING ANTI-STEAL MODULE!`);
+    require('./antisteal.js').run();
+});
+
 // Log in
 client.login(client.config.token);
